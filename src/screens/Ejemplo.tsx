697f2e074f2591504f13713f5f06cef84f29af4d
ncrevelo <nathaliacabrera228@gmail.com>
2023-11-22 15:36:13 -0500

    arreglos para deployment vercel

--- src/screens/Ejemplo.tsx
+++ src/screens/Ejemplo.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from "react";
+//import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
